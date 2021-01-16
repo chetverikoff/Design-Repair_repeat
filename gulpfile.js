d75e9a346e9dd462d900a71bfb5fedd97725ab7d
Anatoly <magistr007.87@gmail.com>
2021-01-17 01:41:27 +0300

    feat:add normalize, grid, variables, header styles in template folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
-var gulp = require('gulp');
+var {src, dest, watch} = require('gulp');
 var browserSync = require('browser-sync').create();
-// Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    gulp.watch("./*.html").on('change', browserSync.reload)
-});
+var sass = require('gulp-sass');
+
+
+function bs() {
+  serveSass();
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
+
+function serveSass() {
+  return src("./sass/**/*.sass", "./sass/**/*.scss")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
